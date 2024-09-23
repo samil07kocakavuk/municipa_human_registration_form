6c9f62d17f7d0cb0f0bdf4c31c34a78a08b55d68
samil07kocakavuk <kocakavuk07samil@gmail.com>
2024-09-23 20:12:05 +0300

    municipa_human_registration_form
    Municipality person registration form works with Access database win_app_form that can register to the database by entering the identity blood group province and district

---
+++ municipa_human_registration_form/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(-2, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(-2, 52);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(242, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(241, 50);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(32, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(38, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.Text = "A+";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(70, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(38, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "B+";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(108, 52);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "AB+";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(153, 52);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(37, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "0+";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Kan Ver";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(273, 7);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(162, 152);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tc";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kan grubu";
+            this.columnHeader2.Width = 88;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(462, 192);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.radioButton5);
+            this.tabPage1.Controls.Add(this.radioButton6);
+            this.tabPage1.Controls.Add(this.radioButton7);
+            this.tabPage1.Controls.Add(this.radioButton8);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton3);
+            this.tabPage1.Controls.Add(this.radioButton4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(454, 166);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Kan Grubu Ve Tc";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(32, 75);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(35, 17);
+            this.radioButton5.TabIndex = 17;
+            this.radioButton5.Text = "A-";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(70, 75);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(35, 17);
+            this.radioButton6.TabIndex = 18;
+            this.radioButton6.Text = "B-";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(108, 75);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(42, 17);
+            this.radioButton7.TabIndex = 19;
+            this.radioButton7.Text = "AB-";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(153, 75);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(34, 17);
+            this.radioButton8.TabIndex = 20;
+            this.radioButton8.Text = "0-";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Kan Al";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.comboBox3);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(454, 166);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "İl İlçe";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 204);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+    }
+}
+
---
+++ municipa_human_registration_form/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=cbbox1.accdb");
+        private void cb2()
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "select ilce from ilceler where il = '" + comboBox1.Text + "'";
+            OleDbDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                comboBox2.Items.Add(oku["ilce"]);
+            }
+            baglanti.Close();
+        }
+        private void cb1_doldur()
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "select il from iller";
+            OleDbDataReader oku = komut.ExecuteReader();
+            comboBox1.Items.Clear();
+            while (oku.Read())
+            {
+                comboBox1.Items.Add(oku["il"]);
+            }
+            baglanti.Close();
+        }
+        private void lwkaydet() 
+        {
+            string kang = "";
+            if (radioButton1.Checked)
+            {
+                kang = "A+";
+            }
+            else if (radioButton2.Checked)
+            {
+                kang = "B+";
+            }
+            else if (radioButton3.Checked)
+            {
+                kang = "AB+";
+            }
+            else if (radioButton4.Checked)
+            {
+                kang = "0+";
+            }
+            else if (radioButton5.Checked)
+            {
+                kang = "A-";
+            }
+            else if (radioButton6.Checked)
+            {
+                kang = "B-";
+            }
+            else if (radioButton7.Checked)
+            {
+                kang = "AB-";
+            }
+            else if (radioButton8.Checked)
+            {
+                kang = "0-";
+            }
+            else
+            {
+                MessageBox.Show("Ya seç işte alla alla");
+                return;
+            }
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into kan (tcno,kang) values('" + textBox3.Text + "','" + kang.ToString() +"')";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            lwdoldur();
+        }
+        private void cb3_doldur()
+        {
+            
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "select il from iller ";
+            OleDbDataReader oku = komut.ExecuteReader();
+            comboBox3.Items.Clear();
+            while (oku.Read())
+            {
+                comboBox3.Items.Add(oku["il"]);
+            }
+            baglanti.Close();
+        }
+        private void lwdoldur()
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "select * from kan ";
+            OleDbDataReader oku = komut.ExecuteReader();
+            listView1.Items.Clear();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["tcno"].ToString();
+                ekle.SubItems.Add(oku["kang"].ToString());
+                listView1.Items.Add(ekle);
+            } baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb1_doldur();
+            lwdoldur();
+            cb3_doldur();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cb2();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into iller (il) values('" + textBox1.Text + "')";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            cb1_doldur();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into ilceler (il,ilce) values('" + comboBox3.Text + "','" + textBox2.Text + "')";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            cb2();
+            cb1_doldur();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into ilceler (il,ilce) values('" + textBox1.Text + "')";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            cb2();
+            cb3_doldur();
+            
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "insert into ilceler (il,ilce) values('" + comboBox3.Text + "','" + textBox2.Text + "')";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            cb2();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "31")
+            {
+                while (true)
+                {
+                    MessageBox.Show("ssjjsjsjsjsjsjsjsjssjjsjs");
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            lwkaydet();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "delete from kan where tcno = '"+textBox3.Text+"'";
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            lwdoldur();
+            
+        }
+
+        
+    }
+}
+// int sifresi programı yaz miss
+//ne ise yarıcak
+// sadece qr okutup siparis verenler baglancak
+//olbilir
+//ama kim ugrascak 
+//valla para verseler ben yrasirim
+//bende
+//200k miss
+//sen bok satarsın 200 işletmeye 
+//valla ayp aga ya
+//tmm yazdında 
+//senın gıbı bu kodu yazan 200 kisi daha var
+//isletme nıye senden alsın 
+//niye almasın
+//200 programdan senınkınımı seçıcek
+//sga 100k kafe olsa 100000/2*1000
+//gene ii para
+//100k kafeyeı nerden bulcan
+//hepsı senı nıye tercıh etsın
+//alana yol silindiri dicem
+//gayet iyi tercih secenegi
+// mıcrosoft veya tesla benzerı sırketlerde çalısmak daha mantıklı daha temız net gelır
+//orda calisirken bunu ek oolarak yapamicani kim soledi
+//tmm ek olarak yazarsında vakıot kaybı 
+//pasif gelir bi sure sonra
+// klasık yatırımcı sozu 
+//"pasıf gelır "
+//valla kardeşim aktif gelir zor iş
+//zor is tmmda net ve temız gelır 
+//kovulmadıgın surece gelmeme ihtimali yok 
+//ve 1 yıl hocanın dedigi kodu yazıp hiç satamazsan napcan 
+//direk kodu sat 10kya fln
+//nsaıl bu kadar emınsın 10 edicegınıe
+//kaynak koldarı cok para ediyo
+//ole para ediyosa ilerde banada sole bende yazip satım 
+//1tb kod arşivi eqwweewqewq
+//hocanın dedığı mantıklı aslında 
+//benım az biraz var kod arşıvım 
+//ediz simdiye kadar yazdıgımız tum kodlar almıs 
+//bendede var arsiv githubda private olarak
+//prıvate ??
+//bana ozel gpzukuyo
+//normal klasore yazsan daha mantıklı
+//niye bukuta kaydedıyon 
+//github gg olsa 
+//senıon kodlar gg 
+//github microsoftun
+//olsun sunucular yine gg olamazmı
+//adamlar saliselik yedek alıyodur
+//iki sunucuda gg olsa 
+//aga cidden 2 sunucuda oldugunumu snaion sadece
+// sunucudan kastım gıthubun verileri kaydettıgı sunucular
+
+/* bana javascrıpt ogret 
+ * olr oretim 
+ ama geççen ogrenme dedin
+ yani istiosan oret in
+ * hoca dil orenin dio ben 6 dil biliom
+ * bende bilmek istıyom 
+ * ama kim ugrascak 
+ * aga napıyosun suadna bos vaktinde
+ * 1000 sorumu cozuyon
+ * kitapmi bitiriyon
+ * witcher oynuyom 
+ * bence yeterlı bı bahane 
+ * sen bilirsin aga
+ * bi sirkete girerken cvne steam profilini eklersin
+ * tabıu 
+ * 300 saat witcher ı olan adamı degilde kimi  alcaklar cd projecte 
+ */
+
+
